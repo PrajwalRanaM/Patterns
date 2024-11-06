06fe5eb90543e1cf31e2a34a290de83ac52cbcdb
Prajwal Rana <prajwalrana15@gmail.com>
2024-11-06 15:01:20 +1100

    adding

---
+++ pattern1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class pattern1 {
+    private static void pattern(int num){
+        int temp=1;
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=i;j++){
+                if(temp%2==0){
+                    System.out.print("0 ");
+                }
+                else
+                    System.out.print("1 ");
+
+                temp++;
+            }
+            System.out.println();
+            
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.print("enter no of rows:");
+        Scanner obj=new Scanner(System.in);
+        int num=obj.nextInt();
+        pattern(num);
+    }  
+}
