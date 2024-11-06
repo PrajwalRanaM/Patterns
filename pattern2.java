bab3c80b4ac01fc317643c3f6155b44f365c6187
Prajwal Rana <prajwalrana15@gmail.com>
2024-11-06 15:59:38 +1100

    adding in github

--- pattern2.java
+++ pattern2.java
@@ -1,4 +1,24 @@
 import java.util.Scanner;
 public class pattern2 {
-    
+    private static void pat2(int num){
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<=num;j++){
+                if(i==1 || i==num|| j==1 ||j==num){
+                    
+                    System.out.print("* ");
+                }
+                else
+                    
+                    System.out.print("  ");
+            }
+           
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.print("enter number of rows: ");
+        int num=obj.nextInt();
+        pat2(num);
+    }
 }
